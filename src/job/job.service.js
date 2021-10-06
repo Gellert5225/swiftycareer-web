7ae7a5a24511cf65545edbf8ba29ec0efda30c68
Gellert5225 <gellert.li1995@gmail.com>
2021-10-05 19:59:40 -0700

    job routes and refactor

--- src/job/job.service.js
+++ src/job/job.service.js
@@ -1,18 +1,25 @@
 const db = require('../server/db');
 const Job = db.database.collection('Job');
 
-exports.getAllJobs = async () => {
+exports.getJobsWithQuery = async (query) => {
+    const pipeline = createMatchPipelineForGetJobs(query);
     const jobs = await Job.aggregate([
         {
             $lookup: {
-                from : "Company",
-                localField: "company",
+                from : "User",
+                localField: "recruiter",
                 foreignField: "_id",
-                as : "company"
+                as : "recruiter"
             }
         },
         {
-            $unwind: "$company"
+            $match: pipeline.$match
+        },
+        {
+            $unwind: "$recruiter"
+        },
+        {
+            $project: { "recruiter.hashed_password": 0 }
         },
         {
             $sort: { "created_at": -1 }
@@ -20,4 +27,42 @@ exports.getAllJobs = async () => {
     ]).toArray();
 
     return jobs;
+}
+
+exports.postJob = async (body) => {
+    const job = {
+        job_title: body.job_title,
+        job_location: body.job_location,
+        job_type: body.job_type,
+        job_description: body.job_description,
+        job_description_short: body.job_description_short,
+        department: body.department,
+        experience_level: body.experience_level,
+        pay_rate: body.pay_rate,
+        is_remote: body.is_remote,
+        recruiter: db.mongodb.ObjectID(body.recruiter),
+        created_at: Date.now()
+    }
+
+    let insert_job_response = (await Job.insertOne(job)).ops[0];
+
+    return insert_job_response;
+}
+
+function createMatchPipelineForGetJobs(query) {
+    const pipeline = {};
+    pipeline.$match = {};
+
+    if (query.jobtype)
+        pipeline.$match["job_type"] = query.jobtype;
+    if (query.exp)
+        pipeline.$match["experience_level"] = query.exp;
+    if (query.payrate)
+        pipeline.$match["pay_rate"] = { $gt: parseInt(query.payrate) };
+    if (query.department)
+        pipeline.$match["department"] = query.department;
+    if (query.remote)
+        pipeline.$match["is_remote"] = (query.remote === "true" ? true : false);
+
+    return pipeline;
 }
