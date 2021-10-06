c8cb5c891e7659c9b8578963657bff3fc2402adf
Gellert5225 <gellert.li1995@gmail.com>
2021-10-05 19:59:40 -0700

    job routes and refactor

--- src/job/job.controller.js
+++ src/job/job.controller.js
@@ -7,9 +7,19 @@ module.exports = function(app) {
 
     app.get('/jobs', async function(req, res) {
         try {
-            const jobs = await jobService.getAllJobs();
+            const jobs = await jobService.getJobsWithQuery(req.query);
             res.status(200).json({ code: 200, info: jobs, error: null });
         } catch (error) {
+            if (!error.status) error.status = 500;
+            res.status(error.status).json({ code: error.status, info: 'error', error: error.message });
+        }
+    });
+
+    app.post('/jobs', async function(req, res) {
+        try {
+            const job = await jobService.postJob(req.body);
+            res.status(200).json({ code: 200, info: job, error: null });
+        } catch (error ){
             if (!error.status) error.status = 500;
             res.status(error.status).json({ code: error.status, info: 'error', error: error.message });
         }
